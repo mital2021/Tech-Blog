39960e020ecef76cc21c74c43da2ea6b257c3dc1
Mital <mitalg0717@gmail.com>
2021-12-19 13:46:51 -0500

    updated server.js

--- server.js
+++ server.js
@@ -3,27 +3,23 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
- const sess = {
-   secret: 'Super secret secret',
+const sess = {
+  secret: 'Super secret secret',
   cookie: {},
-   resave: false,
-   saveUninitialized: true,
-      store: new SequelizeStore({
-   db: sequelize
-   })
- };
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
- app.use(session(sess));
-
- 
- const hbs = exphbs.create({});
+const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -31,9 +27,11 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
- app.use(require('./controllers/'));
+app.use(session(sess));
+app.use((req,res,next) => { res.locals = {...res.locals,...req.session}; next()})
+app.use(require('./controllers/'));
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
+
